0a40f847693e903b12a66d5156373853041c920f
Katherine Solis <ktssolis36@gmail.com>
2026-07-27 17:51:20 -0600

    Config básica personal

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Configurar servidores DNS 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDatabase = async () => {
   const mongoUri = process.env.MONGODB_URI;
